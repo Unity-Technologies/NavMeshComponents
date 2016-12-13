68505e4038eb4d9c82e41a672a9648a4647699e8
Jakob Hunsballe <jakob@unity3d.com>
2016-12-13 11:36:19 +0100

    Remove stray Update of serializedObject. It's the responsibility of the caller. Convert tabs to spaces, was mixed.

--- Assets/NavMeshComponents/Editor/NavMeshComponentsGUIUtility.cs
+++ Assets/NavMeshComponents/Editor/NavMeshComponentsGUIUtility.cs
@@ -3,145 +3,144 @@ using UnityEngine.AI;
 
 namespace UnityEditor.AI
 {
-	public static class NavMeshComponentsGUIUtility
-	{
-   		public static void AreaPopup(string labelName, SerializedProperty areaProperty)
-		{
-			var areaIndex = 0;
-			var areaNames = GameObjectUtility.GetNavMeshAreaNames();
-			for (var i = 0; i < areaNames.Length; i++)
-			{
-				var areaValue = GameObjectUtility.GetNavMeshAreaFromName(areaNames[i]);
-				if (areaValue == areaProperty.intValue)
-					areaIndex = i;
-			}
-			ArrayUtility.Add (ref areaNames, "");
-			ArrayUtility.Add (ref areaNames, "Open Area Settings...");
+    public static class NavMeshComponentsGUIUtility
+    {
+        public static void AreaPopup(string labelName, SerializedProperty areaProperty)
+        {
+            var areaIndex = 0;
+            var areaNames = GameObjectUtility.GetNavMeshAreaNames();
+            for (var i = 0; i < areaNames.Length; i++)
+            {
+                var areaValue = GameObjectUtility.GetNavMeshAreaFromName(areaNames[i]);
+                if (areaValue == areaProperty.intValue)
+                    areaIndex = i;
+            }
+            ArrayUtility.Add (ref areaNames, "");
+            ArrayUtility.Add (ref areaNames, "Open Area Settings...");
 
-			var rect = EditorGUILayout.GetControlRect(true, EditorGUIUtility.singleLineHeight);
-			EditorGUI.BeginProperty(rect, GUIContent.none, areaProperty);
+            var rect = EditorGUILayout.GetControlRect(true, EditorGUIUtility.singleLineHeight);
+            EditorGUI.BeginProperty(rect, GUIContent.none, areaProperty);
 
-			EditorGUI.BeginChangeCheck();
-			areaIndex = EditorGUI.Popup (rect, labelName, areaIndex, areaNames);
+            EditorGUI.BeginChangeCheck();
+            areaIndex = EditorGUI.Popup (rect, labelName, areaIndex, areaNames);
 
-			if (EditorGUI.EndChangeCheck())
-			{
-				if (areaIndex >= 0 && areaIndex < areaNames.Length - 2)
-					areaProperty.intValue = GameObjectUtility.GetNavMeshAreaFromName(areaNames[areaIndex]);
-				else if (areaIndex == areaNames.Length-1)
-					NavMeshEditorHelpers.OpenAreaSettings ();
-			}
+            if (EditorGUI.EndChangeCheck())
+            {
+                if (areaIndex >= 0 && areaIndex < areaNames.Length - 2)
+                    areaProperty.intValue = GameObjectUtility.GetNavMeshAreaFromName(areaNames[areaIndex]);
+                else if (areaIndex == areaNames.Length-1)
+                    NavMeshEditorHelpers.OpenAreaSettings ();
+            }
 
-			EditorGUI.EndProperty();
-		}
+            EditorGUI.EndProperty();
+        }
 
-		public static void AgentTypePopup(string labelName, SerializedProperty agentTypeID)
-		{
-			var index = -1;
-			var count = NavMesh.GetSettingsCount();
-			var agentTypeNames = new string[count + 2];
-			for (var i = 0; i < count; i++)
-			{
-				var id = NavMesh.GetSettingsByIndex (i).agentTypeID;
-				var name = NavMesh.GetSettingsNameFromID (id);
-				agentTypeNames[i] = name;
-				if (id == agentTypeID.intValue)
-					index = i;
-			}
-			agentTypeNames[count] = "";
-			agentTypeNames[count+1] = "Open Agent Settings...";
+        public static void AgentTypePopup(string labelName, SerializedProperty agentTypeID)
+        {
+            var index = -1;
+            var count = NavMesh.GetSettingsCount();
+            var agentTypeNames = new string[count + 2];
+            for (var i = 0; i < count; i++)
+            {
+                var id = NavMesh.GetSettingsByIndex (i).agentTypeID;
+                var name = NavMesh.GetSettingsNameFromID (id);
+                agentTypeNames[i] = name;
+                if (id == agentTypeID.intValue)
+                    index = i;
+            }
+            agentTypeNames[count] = "";
+            agentTypeNames[count+1] = "Open Agent Settings...";
 
-			bool validAgentType = index != -1;
-			if (!validAgentType)
-			{
-				EditorGUILayout.HelpBox ("Agent Type invalid.", MessageType.Warning);
-			}
+            bool validAgentType = index != -1;
+            if (!validAgentType)
+            {
+                EditorGUILayout.HelpBox ("Agent Type invalid.", MessageType.Warning);
+            }
 
-			var rect = EditorGUILayout.GetControlRect(true, EditorGUIUtility.singleLineHeight);
-			EditorGUI.BeginProperty(rect, GUIContent.none, agentTypeID);
+            var rect = EditorGUILayout.GetControlRect(true, EditorGUIUtility.singleLineHeight);
+            EditorGUI.BeginProperty(rect, GUIContent.none, agentTypeID);
 
-			EditorGUI.BeginChangeCheck();
-			index = EditorGUI.Popup (rect, labelName, index, agentTypeNames);
-			if (EditorGUI.EndChangeCheck())
-			{
-				if (index >= 0 && index < count)
-				{
-					var id = NavMesh.GetSettingsByIndex (index).agentTypeID;
-					agentTypeID.intValue = id;
-				}
-				else if (index == count+1)
-				{
+            EditorGUI.BeginChangeCheck();
+            index = EditorGUI.Popup (rect, labelName, index, agentTypeNames);
+            if (EditorGUI.EndChangeCheck())
+            {
+                if (index >= 0 && index < count)
+                {
+                    var id = NavMesh.GetSettingsByIndex (index).agentTypeID;
+                    agentTypeID.intValue = id;
+                }
+                else if (index == count+1)
+                {
                     NavMeshEditorHelpers.OpenAgentSettings(-1);
-				}
-			}
+                }
+            }
 
-			EditorGUI.EndProperty();
-		}
+            EditorGUI.EndProperty();
+        }
 
-		// Agent mask is a set (internally array/list) of agentTypeIDs.
+        // Agent mask is a set (internally array/list) of agentTypeIDs.
         // It is used to describe which agents modifiers apply to.
         // There is a special case of "None" which is an empty array.
         // There is a special case of "All" which is an array of length 1, and value of -1.
-		public static void AgentMaskPopup(string labelName, SerializedProperty agentMask)
-		{
-			// Contents of the dropdown box.
-			string popupContent = "";
+        public static void AgentMaskPopup(string labelName, SerializedProperty agentMask)
+        {
+            // Contents of the dropdown box.
+            string popupContent = "";
 
             if(agentMask.hasMultipleDifferentValues)
                 popupContent = "\u2014";
             else
                 popupContent = GetAgentMaskLabelName(agentMask);
 
-			var content = new GUIContent (popupContent);
-			var popupRect = GUILayoutUtility.GetRect (content, EditorStyles.popup);
+            var content = new GUIContent (popupContent);
+            var popupRect = GUILayoutUtility.GetRect (content, EditorStyles.popup);
 
-			EditorGUI.BeginProperty(popupRect, GUIContent.none, agentMask);
-			popupRect = EditorGUI.PrefixLabel (popupRect, 0, new GUIContent (labelName));
+            EditorGUI.BeginProperty(popupRect, GUIContent.none, agentMask);
+            popupRect = EditorGUI.PrefixLabel (popupRect, 0, new GUIContent (labelName));
             bool pressed = GUI.Button(popupRect, content, EditorStyles.popup);
 
-			if (pressed)
-			{
+            if (pressed)
+            {
                 var show = !agentMask.hasMultipleDifferentValues;
                 var showNone = show && agentMask.arraySize == 0;
                 var showAll = show && IsAll(agentMask);
 
                 var menu = new GenericMenu();
                 menu.AddItem (new GUIContent ("None"), showNone, SetAgentMaskNone, agentMask);
-				menu.AddItem (new GUIContent ("All"), showAll, SetAgentMaskAll, agentMask);
-				menu.AddSeparator ("");
+                menu.AddItem (new GUIContent ("All"), showAll, SetAgentMaskAll, agentMask);
+                menu.AddSeparator ("");
 
-				var count = NavMesh.GetSettingsCount ();
-				for (var i = 0; i < count; i++)
-				{
-					var id = NavMesh.GetSettingsByIndex (i).agentTypeID;
-					var sname = NavMesh.GetSettingsNameFromID(id);
+                var count = NavMesh.GetSettingsCount ();
+                for (var i = 0; i < count; i++)
+                {
+                    var id = NavMesh.GetSettingsByIndex (i).agentTypeID;
+                    var sname = NavMesh.GetSettingsNameFromID(id);
 
                     var showSelected = show && AgentMaskHasSelectedAgentTypeID(agentMask, id);                        
-					var userData = new object[] { agentMask, id, !showSelected };
-					menu.AddItem (new GUIContent (sname), showSelected, ToggleAgentMaskItem, userData);
-				}
+                    var userData = new object[] { agentMask, id, !showSelected };
+                    menu.AddItem (new GUIContent (sname), showSelected, ToggleAgentMaskItem, userData);
+                }
 
-				menu.DropDown (popupRect);
-			}
+                menu.DropDown (popupRect);
+            }
 
             EditorGUI.EndProperty();
-
-		}
+        }
 
         public static GameObject CreateAndSelectGameObject (string suggestedName, GameObject parent)
-		{
-			var parentTransform = parent != null ? parent.transform : null;
-			var uniqueName = GameObjectUtility.GetUniqueNameForSibling (parentTransform, suggestedName);
-			var child = new GameObject (uniqueName);
+        {
+            var parentTransform = parent != null ? parent.transform : null;
+            var uniqueName = GameObjectUtility.GetUniqueNameForSibling (parentTransform, suggestedName);
+            var child = new GameObject (uniqueName);
 
-			Undo.RegisterCreatedObjectUndo (child, "Create " + uniqueName);
-			if (parentTransform != null)
-				Undo.SetTransformParent (child.transform, parentTransform, "Parent " + uniqueName);
+            Undo.RegisterCreatedObjectUndo (child, "Create " + uniqueName);
+            if (parentTransform != null)
+                Undo.SetTransformParent (child.transform, parentTransform, "Parent " + uniqueName);
 
-			Selection.activeGameObject = child;
+            Selection.activeGameObject = child;
 
-			return child;
-		}
+            return child;
+        }
 
         static bool IsAll(SerializedProperty agentMask)
         {
@@ -227,7 +226,6 @@ namespace UnityEditor.AI
 
         static string GetAgentMaskLabelName(SerializedProperty agentMask)
         {
-            agentMask.serializedObject.Update();
             if (agentMask.arraySize == 0)
                 return "None";
 
@@ -264,6 +262,6 @@ namespace UnityEditor.AI
             }
             return false;
         }
-	}
+    }
 }
 
