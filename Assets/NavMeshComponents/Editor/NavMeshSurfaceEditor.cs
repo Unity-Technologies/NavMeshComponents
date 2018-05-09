6bfeeaa660495d6c78add9a3d1c56cff90f65e54
Adrian T <ahskbhsk-code@yahoo.com>
2018-05-09 16:38:43 +0200

    PrefabStageUtility has been moved to the Experimental namespace.

--- Assets/NavMeshComponents/Editor/NavMeshSurfaceEditor.cs
+++ Assets/NavMeshComponents/Editor/NavMeshSurfaceEditor.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using UnityEditor.Experimental.SceneManagement;
 using UnityEditor.IMGUI.Controls;
 using UnityEditor.SceneManagement;
 using UnityEditorInternal;
