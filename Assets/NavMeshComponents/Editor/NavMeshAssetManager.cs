afdebb3d3f77fd8db0a83902b147e91e6c3ae84a
Adrian Turcanu <adriant@unity3d.com>
2020-07-21 13:05:37 +0200

    Update the deprecated use of PrefabStage.prefabAssetPath.

--- Assets/NavMeshComponents/Editor/NavMeshAssetManager.cs
+++ Assets/NavMeshComponents/Editor/NavMeshAssetManager.cs
@@ -41,9 +41,9 @@ namespace UnityEditor.AI
             {
                 var prefabStage = PrefabStageUtility.GetPrefabStage(surface.gameObject);
                 var isPartOfPrefab = prefabStage != null && prefabStage.IsPartOfPrefabContents(surface.gameObject);
-                if (isPartOfPrefab && !string.IsNullOrEmpty(prefabStage.prefabAssetPath))
+                if (isPartOfPrefab && !string.IsNullOrEmpty(prefabStage.assetPath))
                 {
-                    var prefabDirectoryName = Path.GetDirectoryName(prefabStage.prefabAssetPath);
+                    var prefabDirectoryName = Path.GetDirectoryName(prefabStage.assetPath);
                     if (!string.IsNullOrEmpty(prefabDirectoryName))
                         targetPath = prefabDirectoryName;
                 }
