e5a548fce916a226f65a597c73ccf44f5889ee89
gunqqer <gunqqer@friithian.dev>
2022-12-13 21:58:58 -0500

    Add primative support for areas
    Work in progress

--- Assets/Examples/Scripts/NavMeshSourceTag.cs
+++ Assets/Examples/Scripts/NavMeshSourceTag.cs
@@ -1,79 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using System.Collections.Generic;
 
-// Tagging component for use with the LocalNavMeshBuilder
-// Supports mesh-filter and terrain - can be extended to physics and/or primitives
-[DefaultExecutionOrder(-200)]
-public class NavMeshSourceTag : MonoBehaviour
+public class NavMeshSourceTagWithAreas : MonoBehaviour
 {
-    // Global containers for all active mesh/terrain tags
-    public static List<MeshFilter> m_Meshes = new List<MeshFilter>();
-    public static List<Terrain> m_Terrains = new List<Terrain>();
+	public static List<Tuple<MeshFilter, int>> m_Meshes =
+	    new List<Tuple<MeshFilter, int>>();
+	public static List<Tuple<Terrain, int>> m_Terrains =
+	    new List<Tuple<Terrain, int>>();
 
-    void OnEnable()
-    {
-        var m = GetComponent<MeshFilter>();
-        if (m != null)
-        {
-            m_Meshes.Add(m);
-        }
+	[SerializeField]
+	public int areaNum = -1;
 
-        var t = GetComponent<Terrain>();
-        if (t != null)
-        {
-            m_Terrains.Add(t);
-        }
-    }
+	void OnEnable()
+	{
+		if(areaNum == -1) {return;}
+		addToList();
+	}
+	
+	public void addToList()
+	{
+		var m = GetComponent<MeshFilter>();
+		if (m != null)
+		{
+			m_Meshes.Add(new Tuple<MeshFilter, int>(m, areaNum));
+		}
 
-    void OnDisable()
-    {
-        var m = GetComponent<MeshFilter>();
-        if (m != null)
-        {
-            m_Meshes.Remove(m);
-        }
+		var t = GetComponent<Terrain>();
+		if (t != null)
+		{
+			m_Terrains.Add(
+			    new Tuple<Terrain, int>(t, areaNum));
+		}
+	}
 
-        var t = GetComponent<Terrain>();
-        if (t != null)
-        {
-            m_Terrains.Remove(t);
-        }
-    }
+	void OnDisable()
+	{
+		var m = GetComponent<MeshFilter>();
+		if (m != null)
+		{
+			m_Meshes.Remove(new Tuple<MeshFilter, int>(
+			    m, areaNum));
+		}
 
-    // Collect all the navmesh build sources for enabled objects tagged by this component
-    public static void Collect(ref List<NavMeshBuildSource> sources)
-    {
-        sources.Clear();
+		var t = GetComponent<Terrain>();
+		if (t != null)
+		{
+			m_Terrains.Remove(
+			    new Tuple<Terrain, int>(t, areaNum));
+		}
+	}
 
-        for (var i = 0; i < m_Meshes.Count; ++i)
-        {
-            var mf = m_Meshes[i];
-            if (mf == null) continue;
+	public static void Collect(ref List<NavMeshBuildSource> sources)
+	{
+		sources.Clear();
 
-            var m = mf.sharedMesh;
-            if (m == null) continue;
+		for (var i = 0; i < m_Meshes.Count; ++i)
+		{
+			var mf = m_Meshes[i];
+			if (mf.Item1 == null) continue;
 
-            var s = new NavMeshBuildSource();
-            s.shape = NavMeshBuildSourceShape.Mesh;
-            s.sourceObject = m;
-            s.transform = mf.transform.localToWorldMatrix;
-            s.area = 0;
-            sources.Add(s);
-        }
+			var m = mf.Item1.sharedMesh;
+			if (m == null) continue;
 
-        for (var i = 0; i < m_Terrains.Count; ++i)
-        {
-            var t = m_Terrains[i];
-            if (t == null) continue;
+			var s          = new NavMeshBuildSource();
+			s.shape        = NavMeshBuildSourceShape.Mesh;
+			s.sourceObject = m;
+			s.transform    = mf.Item1.transform.localToWorldMatrix;
+			s.area         = mf.Item2;
+			sources.Add(s);
+		}
 
-            var s = new NavMeshBuildSource();
-            s.shape = NavMeshBuildSourceShape.Terrain;
-            s.sourceObject = t.terrainData;
-            // Terrain system only supports translation - so we pass translation only to back-end
-            s.transform = Matrix4x4.TRS(t.transform.position, Quaternion.identity, Vector3.one);
-            s.area = 0;
-            sources.Add(s);
-        }
-    }
+		for (var i = 0; i < m_Terrains.Count; ++i)
+		{
+			var t = m_Terrains[i];
+			if (t.Item1 == null) continue;
+
+			var s          = new NavMeshBuildSource();
+			s.shape        = NavMeshBuildSourceShape.Terrain;
+			s.sourceObject = t.Item1.terrainData;
+			// Terrain system only supports translation - so we pass translation
+			// only to back-end
+			s.transform = Matrix4x4.TRS(
+			    t.Item1.transform.position, Quaternion.identity, Vector3.one);
+			s.area = t.Item2;
+			sources.Add(s);
+		}
+	}
 }
